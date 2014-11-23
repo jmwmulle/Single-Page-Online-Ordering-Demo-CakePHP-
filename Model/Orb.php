ef5da56f301b6d445797d566b9210dfd0cf61ac6
J. Mulle <this.impetus@gmail.com>
2014-11-22 21:06:30 -0400

    Fixing broken merge (conflicts in Route.php not resolved)

--- Model/Orb.php
+++ Model/Orb.php
@@ -64,17 +64,7 @@ class Orb extends AppModel {
 						//'last' => false, // Stop validation after this rule
 						//'on' => 'create', // Limit validation to 'create' or 'update' operations
 					),
-				),
-		'config' => array(
-			'notEmpty' => array(
-				'rule' => array('notEmpty'),
-				//'message' => 'Your custom message here',
-				//'allowEmpty' => false,
-				//'required' => false,
-				//'last' => false, // Stop validation after this rule
-				//'on' => 'create', // Limit validation to 'create' or 'update' operations
-			),
-		),
+				)
 	);
 
 	//The Associations below have been created with all possible keys, those that are not needed can be removed
