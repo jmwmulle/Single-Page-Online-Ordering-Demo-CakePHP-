9193b87b6e1878e74ed2606ff61dc5e2b822983a
EC2 Default User <ec2-user@ip-172-31-42-103.ec2.internal>
2014-12-21 23:07:00 +0000

    Fixed a bug preventing the cart from recognizing when something equivalent had already been added to update quantity.

--- Controller/Component/CartComponent.php
+++ Controller/Component/CartComponent.php
@@ -27,7 +27,7 @@ class CartComponent extends Component {
 
 //////////////////////////////////////////////////
 
-	public function add($orb_id, $quantity = 1, $price_rank = 0, $orbopts_list = null, $prep_instructions = '') {
+	public function add($orb_id, $quantity = 1, $price_rank = 0, $orbopts_list = null, $preparation_instructions = '') {
 
 		if(!is_numeric($quantity)) {
 			$quantity = 1;
@@ -83,16 +83,17 @@ class CartComponent extends Component {
 		$this->Session->write('Test', 'Hello');
 		if ($this->Session->check('Cart.OrderItem')) {
 			$current_data = $this->Session->read('Cart.OrderItem');
-			foreach ($current_data as $key => &$item) {
-				$cur_item = compact($orbopts_list,$price_rank,$orb_id,$prep_instructions);
-				if ($item == $cur_item) { #Consider making this a hash table for speed
-					if ($quantity == 0) {
+			foreach ($current_data as $key => $item) {
+				$cur_item = compact("price_rank","orb_id","preparation_instructions");
+				$cur_item['orbopts_arrangement'] = $orbopts_list;
+				if (array_intersect_key($item, $cur_item) == $cur_item) { #Consider making this a hash table for speed
+					if ($quantity <= 0) {
 						$this->Session->delete('Cart.OrderItem.' . $key);
 					} else {
-						$item['quantity']=$quantity; 
-						$this->Session->write('Cart.OrderItem.' . $key, $current_data);
+						$item['quantity']+=$quantity; 
+						$this->Session->write('Cart.OrderItem.' . $key . '.quantity', $item['quantity']);
 						$matched = True;
-						break;
+						return $item;
 					}
 				}
 			}
@@ -100,25 +101,26 @@ class CartComponent extends Component {
 		if(!$matched && $quantity > 0) {
 			$item['orb_id'] = $orb['Orb']['id'];
 			$item['title'] = $orb['Orb']['title'];
+			$item['description'] = $orb['Orb']['description'];
 			$item['price_rank'] = $price_rank;
 			$item['base_price'] = $prices[$price_rank+1]; #id is index 0, 1-6 are prices/sizes
 			$item['size_name'] = $size_names[$price_rank+1];
 
-			$item['orbopts_ids'] = $orbopts;
+			$item['orbopts'] = $orbopts;
 			$item['orbopts_prices'] = empty($opts_prices) ? array() : $opts_prices;
 			$item['orbopts_arrangement'] = $orbopts_list;
 
-			$item['preparation_instructions'] = $prep_instructions;
+			$item['preparation_instructions'] = $preparation_instructions;
 			$item['quantity'] = $quantity;
 			$item['subtotal'] = sprintf('%01.2f', ($item['base_price'] + array_sum($item['orbopts_prices'])) * $quantity);
 			
-		        $current_data[] = $item;
+			$current_data[] = $item;
+			
 			$this->Session->write('Cart.OrderItem', $current_data);
+			return $item;
 		} else {
 			return false;
 		}
-
-	        return $orb;
 	}
 
 //////////////////////////////////////////////////
@@ -135,7 +137,7 @@ class CartComponent extends Component {
 
 //////////////////////////////////////////////////
 
-	public function cart() {
+	public function update() {
 		$cart = $this->Session->read('Cart');
 		$quantity = 0;
 		$subtotal = 0;
@@ -168,7 +170,7 @@ class CartComponent extends Component {
 			$d['quantity'] = 0;
 			$d['subtotal'] = 0;
 			$d['total'] = 0;
-			$d['deliverable' = false;
+			$d['deliverable'] = false;
 			$this->Session->write('Cart.Order', $d);
 			return false;
 		}
